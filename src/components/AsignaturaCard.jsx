c31ce4c8e5615651e935b2e74126064e66f65458
Julián <julianlopezcastillo12@gmail.com>
2025-01-08 15:41:35 -0400

    LINK

--- src/components/AsignaturaCard.jsx
+++ src/components/AsignaturaCard.jsx
@@ -1,6 +1,9 @@
+import Link from "next/link"
+
 function AsignaturaCard({asignatura}) {
 
   return (
+    < Link href={`/asignaturas/${asignatura._id}`}>
     <div className="bg-grey-800 p-10 text-black rounded-md
     hover:cursor-pointer hover:bg-black-700">
        <h3>{asignatura.name}</h3>
@@ -12,6 +15,7 @@ function AsignaturaCard({asignatura}) {
           <h3>{asignatura.prerequisites}</h3>
           <h3>{asignatura.maximum_quota}</h3> 
     </div>
+    </Link>
   )
 }
 
