b45bc9b5f09f9e36e2473981e7581793c00c2b55
Julián <julianlopezcastillo12@gmail.com>
2025-01-14 22:53:16 -0400

    s

--- src/app/api/asignaturas/[id]/route.js
+++ src/app/api/asignaturas/[id]/route.js
@@ -11,7 +11,7 @@ export async function GET(request, { params }) {
     if (!asignaturaFound)
       return NextResponse.json(
         {
-          message: "Asignayura not found",
+          message: "Asignatura not found",
         },
         {
           status: 404,
